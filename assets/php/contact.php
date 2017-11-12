ada4817a9ace1f3ebe3b2ff95e437419d1f8a7e2
silverehiwario <silverehiwario@gmail.com>
2017-11-12 15:30:21 -0600

    updated my responsive porfolio

---
+++ assets/php/contact.php
@@ -0,0 +1,29 @@
+
+
+
+	$name = $_POST['name'];
+	$email =  $_POST['email'];
+	$message =  $_POST['message'];
+
+
+	$to      = 'silverehiwario@gmail.com';
+	$subject = 'Site Contact Form';
+
+	$headers =  "From: $email \r\n";
+    "Reply-To: $email \r\n" ;
+    'X-Mailer: PHP/' . phpversion();
+
+	$status = mail($to, $subject, $message, $headers);
+
+	if($status == TRUE){
+		$res['sendstatus'] = 'done';
+
+		//Edit your message here
+		$res['message'] = 'Form Submission Successful';
+    }
+	else{
+		$res['message'] = 'Failed to send mail. Please mail me to you@example.com';
+	}
+
+
+	echo($res);
